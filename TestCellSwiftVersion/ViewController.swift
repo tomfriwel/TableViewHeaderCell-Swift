6923d350d5f4aba75636fcedc3269680f82c6bb2
tomfriwel <976932447@qq.com>
2017-04-20 23:11:16 +0800

    Initial Commit

---
+++ TestCellSwiftVersion/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestCellSwiftVersion
+//
+//  Created by tomfriwel on 20/04/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
